fffdbb103adb5d5d788016c2aec1833d1ab27604
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-06-13 20:47:48 +0200

    add: snack ContoBancario

---
+++ src/main/java/org/snack/java/ContoBancario.java
@@ -0,0 +1,28 @@
+package org.snack.java;
+
+public class ContoBancario {
+
+	private int numeroConto;
+	private float saldo;
+
+	public ContoBancario(int numeroConto) {
+
+		this.numeroConto = numeroConto;
+		this.saldo = 0.0F;
+	}
+
+	public void addToConto(float deposito) {
+		this.saldo += deposito;
+
+	}
+
+	public void subFromConto(float prelievo) {
+		this.saldo += prelievo * -1;
+
+	}
+
+	public float getSaldo() {
+		return this.saldo;
+	}
+
+}
--- src/main/java/org/snack/java/Main.java
+++ src/main/java/org/snack/java/Main.java
@@ -5,10 +5,19 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Studente stud1 = new Studente("pippo", "franco", 15);
+//		Studente stud1 = new Studente("pippo", "franco", 15);
+//
+//		System.out.println(stud1.getInfoStudente());
 
-		System.out.println(stud1.infoStudente());
+		ContoBancario conto = new ContoBancario(1);
 
+		System.out.println(conto.getSaldo());
+
+		conto.addToConto(5.0f);
+		System.out.println(conto.getSaldo());
+
+		conto.subFromConto(3.0f);
+		System.out.println(conto.getSaldo());
 	}
 
 }
