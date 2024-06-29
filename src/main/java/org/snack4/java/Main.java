446a8ec0cddb5a8ffdd0df3f66033e1ed1e98223
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-06-29 09:35:00 +0200

    add: elements to List

---
+++ src/main/java/org/snack4/java/Main.java
@@ -0,0 +1,26 @@
+package org.snack4.java;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList<String> newList = new ArrayList<String>();
+
+		newList.add("aldo");
+		newList.add("giovanni");
+		newList.add("giacomo");
+		newList.add("luigi");
+		newList.add("mario");
+		newList.add("gianni");
+		newList.add("marco");
+		newList.add("giulio");
+		newList.add("franco");
+		newList.add("davide");
+
+		System.out.println(newList.toString());
+	}
+
+}
